f01207968928529d7d8161070a5833e0fea8546b
kaddi smail <kaddi.smail1995@gmail.com>
2020-04-07 16:48:15 +0100

    newcommit

---
+++ Untitled-1.js
@@ -0,0 +1,27 @@
+
+
+
+var myChart = document.getElementById('myChart').getContext('2d');
+
+var chart = new chart(myChart,{
+  
+    type : 'line',
+
+    data :{
+    
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+
+    datasets: [{
+
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+    
+    },
+    option:{}
+
+
+}),
+
